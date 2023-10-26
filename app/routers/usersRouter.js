5cd5359ad30411d2d9c39509b531e3c6fc9dc2d1
pascualjordan <pascualjordan@uniovi.es>
2023-10-26 14:24:41 +0200

    first commit

---
+++ app/routers/usersRouter.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const usersController = require('../controllers/usersController')
+
+const usersRouter = express.Router();
+
+usersRouter.post("/login",usersController.loginUser);
+usersRouter.post("/",usersController.postUser);
+usersRouter.get("/disconnect", usersController.disconnect);
+usersRouter.get("/isActiveApiKey", usersController.isActiveApiKey);
+usersRouter.get("/:id", usersController.getUser);
+
+module.exports = usersRouter
